920b63865e1d577fea05da3d103e669dfb36bfed
David Matta <dmatta22201@users.noreply.github.com>
2022-05-11 16:46:48 -0400

    Testing a theory

--- s-wp-config2468.php
+++ s-wp-config2468.php
@@ -88,7 +88,7 @@ define( 'AS3CF_AWS_USE_EC2_IAM_ROLE', true );
 define( 'W3TC_CONFIG_DATABASE', true );
 
 /** to allow https in WordPress, will be used in lab 2 */
-$_SERVER['HTTPS'] = 'on';
+/* $_SERVER['HTTPS'] = 'on'; */
 
 /* That's all, stop editing! Happy publishing. */
 
